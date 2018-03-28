6c547ececf174e15ad5a7058dc9b8af63a2dad84
Marco Milanesi <kpanic@gmail.com>
2018-03-28 23:28:58 +0200

    Simplify the ASCII renderer

--- lib/forecastr/renderer/ascii.ex
+++ lib/forecastr/renderer/ascii.ex
@@ -1,22 +1,25 @@
 defmodule Forecastr.Renderer.ASCII do
-  @hours_to_take [
-    {"09:00:00", "Morning"},
-    {"12:00:00", "Noon"},
-    {"18:00:00", "Afternoon"},
-    {"21:00:00", "Evening"},
-    {"00:00:00", "Night"}
-  ]
+  @relevant_times %{
+    "09:00:00" => "Morning",
+    "12:00:00" => "Noon",
+    "18:00:00" => "Afternoon",
+    "21:00:00" => "Evening",
+    "00:00:00" => "Night"
+  }
 
   @doc "render can be called with [:return :buffer] to avoid printing to stdout"
   def render(weather, opts \\ [])
   @doc "Render today weather condition"
-  def render(%{
-        "name" => name,
-        "sys" => %{"country" => country},
-        "coord" => %{"lat" => lat, "lon" => lon},
-        "weather" => weather,
-        "main" => %{"temp" => temp, "temp_max" => temp_max, "temp_min" => temp_min}
-      }, opts) do
+  def render(
+        %{
+          "name" => name,
+          "sys" => %{"country" => country},
+          "coord" => %{"lat" => lat, "lon" => lon},
+          "weather" => weather,
+          "main" => %{"temp" => temp, "temp_max" => temp_max, "temp_min" => temp_min}
+        },
+        opts
+      ) do
     main_weather_condition = extract_main_weather(weather)
 
     [
@@ -27,67 +30,40 @@ defmodule Forecastr.Renderer.ASCII do
     ]
     |> render(opts)
   end
-
   @doc "Render five days weather condition"
-  def render(%{
-        "city" => %{
-          "name" => name,
-          "country" => country,
-          "coord" => %{"lat" => lat, "lon" => lon}
+  def render(
+        %{
+          "city" => %{
+            "name" => name,
+            "country" => country,
+            "coord" => %{"lat" => lat, "lon" => lon}
+          },
+          "list" => forecast_list
         },
-        "list" => list
-      }, opts)
-      when is_list(list) do
-    weather =
-      group_by_date(list)
-      |> Enum.map(fn {date, forecasts} ->
-        forecasts =
-          forecasts
-          |> group_by_specific_time()
-          |> Enum.reduce([], fn {hour,
-                                 [
-                                   %{
-                                     "weather" => weather,
-                                     "main" => %{
-                                       "temp" => temp,
-                                       "temp_max" => temp_max,
-                                       "temp_min" => temp_min
-                                     }
-                                   }
-                                 ]},
-                                acc ->
-            {_hour, period_of_the_day} =
-              Enum.filter(@hours_to_take, fn {default_hour, _} -> default_hour == hour end)
-              |> List.first()
+        opts
+      )
+      when is_list(forecast_list) do
 
-            main_weather_condition = extract_main_weather(weather)
-
-            [
-              "#{period_of_the_day}\n" <> box(main_weather_condition, temp, temp_max, temp_min)
-              | acc
-            ]
-          end)
-          |> Enum.reverse()
-
-        [Table.table([date], :unicode), Table.table([forecasts], :unicode)]
-      end)
-      |> List.flatten()
+    forecasts =
+      forecast_list
+      |> extract_relevant_times()
+      |> group_by_date()
+      |> prepare_forecasts_for_rendering()
 
     ([
        ~s(Weather report: #{name}, #{country}\n),
        ~s(lat: #{lat}, lon: #{lon}\n),
        "\n"
-     ] ++ weather)
+     ] ++ [forecasts])
     |> render(opts)
   end
-
-  def render(output, []) when is_list(output) do
+  def render(output, return: :buffer) when is_list(output) do
+    output
+  end
+  def render(output, _opts) when is_list(output) do
     IO.write(output)
   end
 
-  def render(output, [return: :buffer]) when is_list(output) do
-    output
-  end
 
   def box(description, temperature, temp_max, temp_min) do
     # TODO: create different ASCII art for different weather conditions :)
@@ -99,6 +75,32 @@ defmodule Forecastr.Renderer.ASCII do
     """
   end
 
+  defp prepare_forecasts_for_rendering(forecasts) do
+    forecasts
+    |> Enum.map(fn {date, forecasts} ->
+      forecasts =
+        Enum.reduce(forecasts, [], fn %{
+          "weather" => weather,
+          "dt_txt" => date_time,
+          "main" => %{
+            "temp" => temp,
+            "temp_max" => temp_max,
+            "temp_min" => temp_min
+          }}, acc ->
+            main_weather_condition = extract_main_weather(weather)
+            time = extract_time(date_time)
+            period_of_the_day = Map.get(@relevant_times, time)
+
+            [
+              "#{period_of_the_day}\n" <> box(main_weather_condition, temp, temp_max, temp_min)
+              | acc
+            ]
+        end)
+        |> Enum.reverse()
+      [Table.table([date], :unicode), Table.table([forecasts], :unicode)]
+    end)
+  end
+
   defp group_by_date(list) do
     Enum.group_by(list, fn element ->
       <<year::bytes-size(4)>> <>
@@ -107,25 +109,25 @@ defmodule Forecastr.Renderer.ASCII do
 
       year <> "-" <> month <> "-" <> day
     end)
-    |> Enum.sort()
   end
 
-  defp group_by_specific_time(forecasts) do
-    hours_to_lookup = Enum.map(@hours_to_take, fn {hour, _} -> hour end)
-
-    Enum.group_by(forecasts, fn element ->
-      <<_year::bytes-size(4)>> <>
-        "-" <> <<_month::bytes-size(2)>> <> "-" <> <<_day::bytes-size(2)>> <> " " <> time =
-        element["dt_txt"]
-
-      time
+  defp extract_relevant_times(forecast_list) do
+    forecast_list
+    |> Enum.filter(fn element ->
+      time = extract_time(element["dt_txt"])
+      time in Map.keys(@relevant_times) == true
     end)
-    |> Map.take(hours_to_lookup)
-    |> Enum.sort()
   end
 
   defp extract_main_weather(weather) do
     %{"description" => main_weather_condition} = List.first(weather)
     main_weather_condition
+  end
+
+  defp extract_time(date) do
+    <<_year::bytes-size(4)>> <>
+      "-" <> <<_month::bytes-size(2)>> <> "-" <> <<_day::bytes-size(2)>> <> " " <> time = date
+
+    time
   end
 end
