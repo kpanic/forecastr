09a633071644fe8d31af54cb2b08ecbb81e944bf
Marco Milanesi <kpanic@gmail.com>
2018-03-31 22:07:54 +0200

    Bump minor version

--- README.md
+++ README.md
@@ -61,7 +61,7 @@ by adding `forecastr` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:forecastr, "~> 0.1.0"}
+    {:forecastr, "~> 0.1.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Forecastr.MixProject do
   def project do
     [
       app: :forecastr,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       description: description(),
       package: package(),
