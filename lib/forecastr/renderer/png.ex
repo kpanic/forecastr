b552692b72583e34495afcf6460251bf3f8d9acc
Marco Milanesi <kpanic@gmail.com>
2018-04-06 21:40:09 +0200

    Add basic PNG renderer

---
+++ lib/forecastr/renderer/png.ex
@@ -0,0 +1,33 @@
+defmodule Forecastr.Renderer.PNG do
+  @moduledoc false
+  import Mogrify
+
+  @spec render(map()) :: String.t()
+  def render(map = %{"name" => city_name}) do
+    map
+    |> Forecastr.Renderer.ASCII.render()
+    |> render_png(city_name)
+  end
+
+  def render(map = %{"city" => %{"name" => city_name}}) do
+    "#{city_name}.png"
+  end
+
+  defp render_png(ascii, city_name) when is_list(ascii) do
+    city_name = String.downcase(city_name)
+
+    filename = "#{city_name}.png"
+    path = Application.fetch_env!(:forecastr, :image_path)
+
+    %Mogrify.Image{path: filename, ext: "png"}
+    |> custom("size", "280x280")
+    |> canvas("black")
+    |> custom("gravity", "center")
+    |> custom("fill", "white")
+    |> custom("font", "FreeMono")
+    |> custom("draw", "text 0,0 '#{ascii}'")
+    |> create(path: path)
+
+    filename
+  end
+end
