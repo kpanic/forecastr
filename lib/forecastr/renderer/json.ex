094cb864323a5ee34906bddd89c6809a8cd0066e
Marco Milanesi <kpanic@gmail.com>
2018-04-09 20:53:58 +0200

    Add naked JSON renderer

---
+++ lib/forecastr/renderer/json.ex
@@ -0,0 +1,12 @@
+defmodule Forecastr.Renderer.JSON do
+
+  @moduledoc """
+  JSON "renderer".
+  Currently it just returns what the OWM API will return
+  """
+
+  @spec render(map()) :: map()
+  def render(forecast) do
+    forecast
+  end
+end
