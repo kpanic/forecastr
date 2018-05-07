99a7e1c64621afcb623b6e797b0fe73542646756
Marco Milanesi <kpanic@gmail.com>
2018-05-07 18:15:23 +0200

    Refine Colours module

--- lib/forecastr/renderer/colours.ex
+++ lib/forecastr/renderer/colours.ex
@@ -5,7 +5,55 @@ defmodule Forecastr.Renderer.Colours do
     [ANSI.bright(), ANSI.yellow()]
   end
 
+  def bright_yellow(:png) do
+    [~S(<span foreground="yellow">)]
+  end
+
+  def yellow(:ansi) do
+    [ANSI.yellow()]
+  end
+
+  def yellow(:png) do
+    [~S(<span foreground="yellow">)]
+  end
+
+  def magenta(:ansi) do
+    [ANSI.light_magenta()]
+  end
+
+  def magenta(:png) do
+    [~S(<span foreground="magenta">)]
+  end
+
+  def white(:ansi) do
+    [ANSI.white()]
+  end
+
+  def white(:png) do
+    [~S(<span foreground="white">)]
+  end
+
+  def light_white(:ansi) do
+    [ANSI.light_white()]
+  end
+
+  def light_white(:png) do
+    [~S(<span foreground="white">)]
+  end
+
+  def blue(:ansi) do
+    [ANSI.blue()]
+  end
+
+  def blue(:png) do
+    [~S(<span foreground="blue">)]
+  end
+
   def reset(:ansi) do
     [ANSI.reset()]
+  end
+
+  def reset(:png) do
+    ["</span>"]
   end
 end
