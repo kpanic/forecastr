efb87f08b5e590aa9987e5ff489f612498dd6f7f
Marco Milanesi <kpanic@gmail.com>
2018-11-18 18:53:26 +0100

    Remove dup specs

--- lib/forecastr/http.ex
+++ lib/forecastr/http.ex
@@ -3,10 +3,8 @@ defmodule Forecastr.HTTP do
 
   use HTTPoison.Base
 
-  @spec process_url(String.t()) :: String.t()
   def process_request_url(url), do: "http://" <> url
 
-  @spec process_request_options(Keyword.t()) :: Keyword.t()
   def process_request_options(options),
     do:
       Keyword.merge(
