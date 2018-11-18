0a4bb06ace9b58fa09d07a30ca3696893d740493
Marco Milanesi <kpanic@gmail.com>
2018-11-18 17:47:43 +0100

    Bump deps

--- mix.exs
+++ mix.exs
@@ -31,12 +31,12 @@ defmodule Forecastr.MixProject do
 
   defp deps do
     [
-      {:httpoison, "~> 1.1"},
+      {:httpoison, "~> 1.4"},
       {:poison, "~> 3.1"},
       {:elbat, "~> 0.0.6"},
-      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
-      {:credo, "~> 0.9.0-rc1", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.8", only: :test},
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+      {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:mogrify, "~> 0.6.0"}
     ]
