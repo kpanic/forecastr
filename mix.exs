94e610c9046d73a74d41ae29889bd12f13d13051
Marco Milanesi <kpanic@gmail.com>
2019-04-14 15:50:48 +0200

    Bump to 0.1.11

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Forecastr.MixProject do
   def project do
     [
       app: :forecastr,
-      version: "0.1.10",
+      version: "0.1.11",
       elixir: "~> 1.7",
       description: description(),
       package: package(),
