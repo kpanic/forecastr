2d67468588ec2d7b2d56bddb3116a34704da8574
Marco Milanesi <kpanic@gmail.com>
2019-04-22 08:49:52 +0200

    Bump to 0.2.1 (PNG tuple shape corrected)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Forecastr.MixProject do
   def project do
     [
       app: :forecastr,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.7",
       description: description(),
       package: package(),
