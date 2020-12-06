2a2cefa4a714e0483f59a38bb4e79b739bffa136
Marco Milanesi <kpanic@gmail.com>
2020-12-06 11:18:32 +0100

    Move Giphy.HTTP module in its own file

--- lib/forecastr/renderer/giphy.ex
+++ lib/forecastr/renderer/giphy.ex
@@ -13,40 +13,9 @@ defmodule Forecastr.Renderer.Giphy do
 
   alias __MODULE__
 
-  defmodule HTTP do
-    @number_of_gifs_to_dig 50
-    @moduledoc false
-    @api_key Application.get_env(:forecastr, :giphy_api_key)
-
-    def search(keyword) do
-      query =
-        URI.encode_query(%{
-          q: keyword,
-          api_key: @api_key,
-          limit: @number_of_gifs_to_dig
-        })
-
-      giphy_url = "https://api.giphy.com/v1/gifs/search?#{query}"
-
-      giphy_url
-      |> HTTPoison.get!()
-      |> Map.get(:body)
-      |> Poison.decode!()
-      |> extract_gif_urls()
-    end
-
-    defp extract_gif_urls(%{"data" => gifs}) do
-      Enum.map(gifs, fn gif ->
-        %{"images" => %{"fixed_height" => %{"url" => url}}} = gif
-        String.replace(url, ~r/(.*)(media\d+)(.*)/, "\\1i\\3")
-      end)
-    end
-  end
-
   @spec render(map()) :: map()
   def render(%{"description" => description} = map) do
-    map
-    |> Map.put("giphy_pic", Enum.random(Giphy.HTTP.search(description)))
+    Map.put(map, "giphy_pic", Enum.random(Giphy.HTTP.search(description)))
   end
 
   def render(%{"list" => forecasts} = map) do
